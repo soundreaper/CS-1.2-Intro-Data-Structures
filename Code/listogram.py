90322514773b71b8eceef3cc66cc415c6efb9cb3
soundreaper@gmail.com <soundreaper@gmail.com>
2019-11-13 15:03:30 -0800

    implement functions for listogram

--- Code/listogram.py
+++ Code/listogram.py
@@ -21,25 +21,53 @@ class Listogram(list):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if self.__contains__(word):
+            self[self.index_of(word)][1] += count
+        else:
+            self.append([word, count])
+            self.types += 1
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        for i in self:
+            if i[0] == word:
+                return i[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for i in self:
+            if i[0] == word:
+                return True
+        return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        for index in range(len(self)):
+            if self[index][0] == target:
+                return index
+        return None
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        word_distribution = []
+        range_start = 0
+        for word in self:
+            frequency_percent = word[1] / self.tokens
+            word_distribution.append([word[0], [range_start, range_start + frequency_percent]])
+            range_start += frequency_percent
 
+        random_num = random.random()
+        for index in range(len(word_distribution)):
+            if word_distribution[index][1][0] <= random_num <= word_distribution[index][1][1]:
+                return word_distribution[index][0]
 
 def print_histogram(word_list):
     print()
